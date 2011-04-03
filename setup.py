b71212e4dddffdf82db3305483d1db1627c82a4d
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-03 23:25:13 +0200

    Pimp up the setup script

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ from setuptools import setup
 setup(
     name='Flask-Attest',
     version='0.1dev',
+    description='Test Flask applications with Attest',
+    url='https://github.com/dag/flask-attest',
+
+    author='Dag Odenhall',
+    author_email='dag.odenhall@gmail.com',
+    license='Simplified BSD',
 
     packages=['flaskext'],
     namespace_packages=['flaskext'],
+    zip_safe=False,
 
     install_requires=[
         'Flask>=0.6',
@@ -16,10 +23,17 @@ setup(
 
     test_loader='attest:auto_reporter.test_loader',
     test_suite='tests.app',
-    tests_require=[
-        'Attest>=0.5',
-        'Flask-Genshi>=0.5.1'
-    ],
+    tests_require=['Flask-Genshi>=0.5.1'],
 
-    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Testing',
+    ]
 )
