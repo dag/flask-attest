7837d4cb67da4904d79672a8003875eeb31a5adf
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-03 11:50:40 +0200

    Make tests run on CPython 2.5 and PyPy

--- setup.py
+++ setup.py
@@ -14,5 +14,12 @@ setup(
         'decorator',
     ],
 
+    test_loader='attest:auto_reporter.test_loader',
+    test_suite='tests.app',
+    tests_require=[
+        'Attest>=0.5',
+        'Flask-Genshi>=0.5.1'
+    ],
+
     zip_safe=False,
 )
