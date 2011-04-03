22bcf020ac233fc9f3f66f67d26400aa295566f8
Dag Odenhall <dag.odenhall@gmail.com>
2011-04-03 15:41:01 +0200

    Intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,13 @@ copyright = u'2011, Dag Odenhall'
 version = '0.1'
 release = '0.1'
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+intersphinx_mapping = {
+    'http://docs.python.org/': None,
+    'http://flask.pocoo.org/docs/': None,
+    'http://werkzeug.pocoo.org/docs/': None,
+    'http://packages.python.org/Attest/': None,
+}
 
 master_doc = 'index'
 
